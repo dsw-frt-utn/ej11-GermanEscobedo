6ba70f7909d720a4da1fbabae0b866fd6e051dd1
unknown <lautaro.escobedo@alu.frt.utn.edu.ar>
2026-05-22 16:44:43 -0600

    se realizan las consignas de la clase CasoDictionary

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -1,10 +1,43 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using System.Collections.Generic;
+using Dsw2026Ej11.Domain;
 
-//Crear un diccionario donde la clave sea el legajo y el valor el alumno
-//Incluir un método para agregar un alumno al diccionario
-//Incluir un método para buscar un alumno utilizando la clave
-//Incluir un método para retornar el diccionario
-//Incluir un método para eliminar un alumno utilizando la clave
+namespace Dsw2026Ej11.Collections;
+
 public class CasoDictionary
 {
+    //Crear un diccionario donde la clave sea el legajo y el valor el alumno
+    private Dictionary<int, Alumno> _alumnos = new Dictionary<int, Alumno>();
+
+    //Incluir un método para agregar un alumno al diccionario
+    public void AgregarAlumno(Alumno alumno)
+    {
+        // Usamos ContainsKey para verificar que el Id no esté registrado previamente. 
+        // Si intentamos agregar una clave duplicada en un diccionario, el programa lanzará una excepción.
+        if (!_alumnos.ContainsKey(alumno.Id))
+        {
+            _alumnos.Add(alumno.Id, alumno);
+        }
+    }
+
+    //Incluir un método para buscar un alumno utilizando la clave
+    public Alumno BuscarPorClave(int legajo)
+    {
+        // en la variable 'alumnoEncontrado'. Si no existe, simplemente retorna null sin romper el programa.
+        _alumnos.TryGetValue(legajo, out Alumno alumnoEncontrado);
+        return alumnoEncontrado;
+    }
+
+    //Incluir un método para retornar el diccionario
+    public Dictionary<int, Alumno> ObtenerDiccionario()
+    {
+        return _alumnos;
+    }
+
+    //Incluir un método para eliminar un alumno utilizando la clave
+    public void EliminarPorClave(int legajo)
+    {
+        // Remove busca directamente la clave. Si existe, la elimina junto con su valor y  si no existe la elimina 
+        
+        _alumnos.Remove(legajo);
+    }
 }
--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,4 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -6,6 +6,50 @@
 //Incluir un método para buscar un alumno por nombre
 //Incluir un método para eliminar un alumno (debe recibir un alumno)
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
+using Dsw2026Ej11.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dsw2026Ej11.Collections;
+
 public class CasoList
 {
+    //Crear un campo que represente una lista de alumnos (List<>)
+    private List<Alumno> _alumnos = new List<Alumno>();
+
+    //Incluir un método para agregar alumnos a la lista
+    public void AgregarAlumno(Alumno alumno)
+    {
+        _alumnos.Add(alumno);
+    }
+
+    //Incluir un método para retornar la lista
+    public List<Alumno> ObtenerLista()
+    {
+        return _alumnos;
+    }
+
+    //Incluir un método para buscar un alumno por nombre
+    public Alumno BuscarPorNombre(string nombre)
+    {
+        return _alumnos.FirstOrDefault(a => a.Nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase));
+    }
+
+    //Incluir un método para eliminar un alumno (debe recibir un alumno)
+    public void EliminarAlumno(Alumno alumno)
+    {
+        _alumnos.Remove(alumno);
+    }
+
+    //Incluir un método para eliminar un alumno en una determinada posición de la lista
+    public void EliminarPorPosicion(int posicion)
+    {
+        if (posicion >= 0 && posicion < _alumnos.Count)
+        {
+            _alumnos.RemoveAt(posicion);
+        }
+    }
 }
+
+
